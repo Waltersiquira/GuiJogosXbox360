80a71308a5d2f0bb6c3378e781363bad8824242e
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2025-01-16 11:53:51 -0300

    Create comprar-jogo-xbox-360.php

---
+++ comprar-jogo-xbox-360.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="icons/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=arrow_back_ios" />
+    <title>GuiJogosXbox360</title>
+</head>
+<body style="text-align: center;">
+    <h1>Aprovado</h1>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/8/84/Approved_img.png" alt="">
+    <p>Jogo de Xbox 360 Comprado com Sucesso</p>
+    <button><a href="index.php"><span class="material-symbols-outlined">arrow_back_ios</span></a></button>
+</body>
+</html>
